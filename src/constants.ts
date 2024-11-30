cc6824e15ffd8a0672f8f41de4063763c9a3102c
DenisGaig <denis_gaignard@yahoo.fr>
2024-11-30 14:39:32 +0100

    v1 le feu et l'eau

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Le feu et l'eau - Poésie de l'âme";
+export const SITE_DESCRIPTION = "Poèmes sur l'amour ";
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 3;
